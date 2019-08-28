810f28e86d01daf2d678f2e8b7f04f8503ef2932
younesDiv00 <13768282@student.uts.edu.au>
2019-08-28 16:37:06 +1000

    folder delete test

---
+++ netnexus/resources/functions.php
@@ -0,0 +1,225 @@
+<?php
+
+//helper functions
+
+function destroy_sess()
+{
+    session_destroy();
+}
+
+function set_message($msg)
+{
+    if(!empty($msg))
+    {
+        $_SESSION['message'] = $msg;
+    }else{
+        $msg = "";
+    }
+}
+
+function display_message()
+{
+    if(isset($_SESSION['message']))
+    {
+        echo $_SESSION['message'];
+        unset($_SESSION['message']);
+    }
+}
+
+function redirect($location){
+
+    header("Location: $location ");
+}
+
+function query($sql){
+
+    global $conn;
+    return mysqli_query($conn, $sql);
+}
+
+function confirm($result){
+    if(!$result)
+    {
+        die("QUERY FAILED" . mysqli_error($conn));
+    }
+}
+
+function escape_string($string){
+    global $conn;
+    return mysqli_real_escape_string($conn, $string);
+}
+
+function fetch_array($result)
+{
+    return mysqli_fetch_array($result);
+}
+
+
+function get_products()
+{
+   $query = query(" SELECT * FROM products");
+   confirm($query);
+
+    while($row = fetch_array($query))
+    {
+        $product = <<<DELIMETER
+
+            <div class="col-sm-4 col-lg-4 col-md-4">
+                <div class="thumbnail" style="height:340px;">
+                    <a href="item.php?id={$row['product_id']}"><img style="width: auto;height:165px;" class="imgsize" src="{$row['product_image']}" alt=""></a>
+                    <div class="caption">
+                        <h4 style="overflow: hidden;text-overflow: ellipsis;" >
+                            <a style="text-overflow: ellipsis;" href="item.php?id={$row['product_id']}">{$row['product_title']}</a>
+                        </h4>
+                        <p style="overflow: hidden;text-overflow: ellipsis;">{$row['product_short_description']}</p>
+                    </div>
+                    <div class="ratings">
+                        <a class="btn btn-primary" target="_blank" href="cart.php?add={$row['product_id']}">Add To Cart</a>
+                        <h4 class="pull-right">&#36;{$row['product_price']}</h4>
+                    </div>
+                </div>
+            </div>
+
+        DELIMETER;
+
+        echo $product;
+    }
+}
+
+
+function get_categories_products()
+{
+   $query = query(" SELECT * FROM products WHERE product_category_id =" . escape_string($_GET['id']) ."");
+   confirm($query);
+
+    while($row = fetch_array($query))
+    {
+        $product = <<<DELIMETER
+
+            <div class="col-md-4 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <a href="item.php?id={$row['product_id']}"><img style="width: auto;height:200px;" class="imgsize" src="{$row['product_image']}" alt=""></a>
+                    <div class="caption">
+                        <h4 style="overflow: hidden;text-overflow: ellipsis;" >
+                            <a style="text-overflow: ellipsis;" href="item.php?id={$row['product_id']}">{$row['product_title']}</a>
+                        </h4>
+                        <p style="overflow: hidden;text-overflow: ellipsis;">{$row['product_short_description']}</p>
+                    </div>
+                    <div class="ratings">
+                        <a class="btn btn-primary" target="_blank" href="cart.php?add={$row['product_id']}">Add To Cart</a>
+                    </div>
+                </div>
+            </div>
+
+        DELIMETER;
+
+        echo $product;
+    }
+}
+
+
+function get_categories()
+{
+    $query = query("SELECT * FROM categories");
+    confirm($query);
+
+    while($row = mysqli_fetch_array($query))
+    {
+       $categories = <<<DELIMETER
+            <a href="category.php?id={$row['cat_id']}" class='list-group-item'>{$row['cat_title']}</a>
+        DELIMETER;
+
+        echo $categories;
+    }
+}
+
+
+function  get_shop_products()
+{
+   $query = query(" SELECT * FROM products");
+   confirm($query);
+
+    while($row = fetch_array($query))
+    {
+        $product = <<<DELIMETER
+
+            <div class="col-md-4 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <a href="item.php?id={$row['product_id']}"><img style="width: auto;height:200px;" class="imgsize" src="{$row['product_image']}" alt=""></a>
+                    <div class="caption">
+                        <h4 style="overflow: hidden;text-overflow: ellipsis;" >
+                            <a style="text-overflow: ellipsis;" href="item.php?id={$row['product_id']}">{$row['product_title']}</a>
+                        </h4>
+                        <p style="overflow: hidden;text-overflow: ellipsis;">{$row['product_short_description']}</p>
+                    </div>
+                    <div class="ratings">
+                        <p>
+                        <a class="btn btn-primary" target="_blank" href="cart.php?add={$row['product_id']}">Add To Cart</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        DELIMETER;
+
+        echo $product;
+    }
+}
+
+
+function login_user()
+{
+    if(isset($_POST['submit']))
+    {
+        $username = escape_string($_POST['username']);
+        $password = escape_string($_POST['password']);
+
+        $query = query("SELECT * FROM users WHERE username = '{$username}' AND password = '{$password}'");
+        confirm($query);
+
+        if(mysqli_num_rows($query) == 0)
+        {
+            set_message("incorrect password or username");
+            redirect("login.php");
+        }else
+        {
+            set_message("welcome to admin {$username}");
+            redirect("admin");
+        }
+    }
+}
+
+
+
+function send_message()
+{
+    if(isset($_POST['submit']))
+    {
+        $to        = "someEmail@gmail.com";
+        $from_name = $_POST["name"];
+        $subject   = $_POST["subject"];
+        $email     = $_POST["email"];
+        $message   = $_POST["message"];
+        $headers = "From: {$from_name} {$email}";
+
+        //should change very unreliable
+        $res = mail($to,$subject,$message,$headers);
+
+        if(!res)
+        {
+            set_message("error");
+            redirect("contact.php");
+        }else
+        {
+            set_message("success");
+            redirect("contact.php");
+
+        }
+    }
+}
+
+
+
+
+
+//____________________________________ back end functions__________________//

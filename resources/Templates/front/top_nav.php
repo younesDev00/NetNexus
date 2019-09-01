46a9b85f0d436efc40ade631e5dc7f235dceee60
zetovicmarko1 <zetovicmarko1@gmail.com>
2019-09-01 21:22:23 +1000

    Logo Added

--- resources/Templates/front/top_nav.php
+++ resources/Templates/front/top_nav.php
@@ -1,13 +1,16 @@
   <div class="container">
       <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
+
           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
               <span class="sr-only">Toggle navigation</span>
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php">Home</a>
+          <a class="navbar-brand" href="index.php">
+          <img src="https://i.imgur.com/kaBX8fL.png" alt="">
+        </a>
       </div>
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">

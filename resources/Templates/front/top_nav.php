8438ead74f55c778044f08fd2536883f3f4550b5
Gabrielle Walker <13196732@student.uts.edu.au>
2019-09-03 10:01:11 +1000

    adding search bar?

--- resources/Templates/front/top_nav.php
+++ resources/Templates/front/top_nav.php
@@ -1,49 +1,51 @@
-  <div class="container">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
+<link href="css/styles.css" rel="stylesheet">
 
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="index.php">
-          <img src="https://i.imgur.com/kaBX8fL.png" alt="">
+<div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php">
+            <img src="https://i.imgur.com/kaBX8fL.png" alt="">
         </a>
-      </div>
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
-              <li>
-                  <a href="shop.php">Shop</a>
-              </li>
-              <li>
-                  <a href="login.php">Login</a>
-              </li>
-              <li>
-                  <a href="admin">Admin</a>
-              </li>
-              <li>
-                  <a href="checkout.php">Checkout</a>
-              </li>
-              <li>
-                  <a href="contact.php">Contact</a>
-              </li>
-              <li>
-                  <a href="register.php">Register</a>
-              </li>
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+            <li>
+                <a href="shop.php">Shop</a>
+            </li>
+            <li>
+                <a href="login.php">Login</a>
+            </li>
+            <li>
+                <a href="admin">Admin</a>
+            </li>
+            <li>
+                <a href="checkout.php">Checkout</a>
+            </li>
+            <li>
+                <a href="contact.php">Contact</a>
+            </li>
+            <li>
+                <a href="register.php">Register</a>
+            </li>
 
-          </ul>
+        </ul>
+        <div class="searchbar">
+            <form class="form-inline my-2 my-lg-0" >
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
 
-          <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+    </div>
 
-
-      </div>
-
-      <!-- /.navbar-collapse -->
-  </div>
-  <!-- /.container -->
+    <!-- /.navbar-collapse -->
+</div>
+<!-- /.container -->

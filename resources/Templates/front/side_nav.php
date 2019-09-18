8b310bee95a6e71a8fedf51d6299182332829e1f
Gabrielle Walker <13196732@student.uts.edu.au>
2019-09-18 17:52:59 +1000

    side navigation fix

--- resources/Templates/front/side_nav.php
+++ resources/Templates/front/side_nav.php
@@ -1,10 +1,22 @@
+<link href="css/styles.css" rel="stylesheet">
+
 <div class="col-md-2">
-    <p class="lead">Shop Name</p>
+    <h2 class="sidebarheading">Shop</h2>
     <div class="list-group">
 
-    	<?php
+        <?php get_categories(); ?>
+    </div>
 
-    		get_categories();
-    	 ?>
+
+    <div class="list-group"  >
+        <h3 >Shop by Price</h3>
+        <div   >
+                <a href="shop.php?lowPrice=0&highPrice=500&search=" class="list-group-item">Less than $500</a>
+                <a href="shop.php?lowPrice=500&highPrice=1000&search=" class="list-group-item">$500-$1000</a>
+                <a href="shop.php?lowPrice=1000&highPrice=2000&search=" class="list-group-item">$1000-$2000</a>
+                <a href="shop.php?lowPrice=2000&highPrice=3000&search=" class="list-group-item">$2000-$3000</a>
+                <a href="shop.php?lowPrice=3000&highPrice=4000&search=" class="list-group-item">$3000-$4000</a>
+                <a href="shop.php?lowPrice=4000&highPrice=10000&search=" class="list-group-item">$4000+</a>
+        </div>
     </div>
 </div>

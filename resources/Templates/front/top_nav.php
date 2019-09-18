1e4dc257fe0fffaa1e2e856c82300a3125ab8369
zetovicmarko1 <zetovicmarko1@gmail.com>
2019-09-18 17:10:00 +1000

    master fix

--- resources/Templates/front/top_nav.php
+++ resources/Templates/front/top_nav.php
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-<link href="css/styles.css" rel="stylesheet">
-
-<div class="container">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="index.php">
-            <img src="https://i.imgur.com/kaBX8fL.png" alt="" >
-        </a>
-    </div>
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-            <li>
-                <a href="shop.php?lowPrice&highPrice&search">Shop</a>
-            </li>
-            <li>
-                <a href="login.php">Login</a>
-            </li>
-            <li>
-                <a href="admin">Admin</a>
-            </li>
-            <li>
-                <a href="checkout.php">Checkout</a>
-            </li>
-            <li>
-                <a href="contact.php">Contact</a>
-            </li>
-            <li>
-                <a href="register.php">Register</a>
-            </li>
-
-        </ul>
-
-        <form class="search"  action="shop.php?lowPrice&highPrice&search" method="get">
-            <input type="hidden" placeholder="Product Search" name="lowPrice">
-            <input type="hidden" placeholder="Product Search" name="highPrice">
-            <input type="text" placeholder="Product Search" name="search">
-            <button type="submit" >Search</button>
-        </form>
-
-
-
-=======
 <nav class="navbar navbar-inverse navbar-fixed-top" >
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
@@ -70,7 +19,6 @@
                 <li>
                     <a href="shop.php">Shop</a>
                 </li>
->>>>>>> master
 
                 <?php if(isset($_SESSION['useraccount'])) { echo '<li><a href="checkout.php">Checkout</a></li>'; } ?>
 
@@ -88,13 +36,6 @@
                         echo '<form style="padding:10px;float:left;" method="post"><li><a><input type="submit"  class="btn btn-danger" name="logout" value="logout"></a></li></form>' .signout() .' ';
         //                echo $_SESSION['useraccount'][1];
 
-<<<<<<< HEAD
-</div>
-
-<!-- /.navbar-collapse -->
-</div>
-<!-- /.container -->
-=======
                     }else
                     {
                         echo '<li><a href="/shoppingtest/public/admin/index.php?main">Welcome ' .$_SESSION['useraccount'][0] .' </a></li>';
@@ -121,4 +62,3 @@
     </div>
     <!-- /.container -->
 </nav>
->>>>>>> master

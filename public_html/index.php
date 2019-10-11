0b584cd344899bf96e3bcda2af3987929530c929
Gabrielle Walker <13196732@student.uts.edu.au>
2019-10-11 17:32:58 +1100

    carousel added, however need to link image to item.php

--- public_html/index.php
+++ public_html/index.php
@@ -17,57 +17,65 @@ include(TEMPLATE_FRONT . DS . "header.php")
         <!-- /.col-lg-3 -->
 
         <div class="col-lg-9">
-
-            <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner" role="listbox">
-                    <div class="carousel-item active">
-                        <img style="display: block; margin-left: auto; margin-right: auto; width: 40%;" class="d-block img-fluid" src="resources/uploads/dell2.png" alt="First slide">
-                            <h3>Dell Inspiron 3000</h3>
-                    </div>
-                    <div class="carousel-item">
-                        <img style="display: block; margin-left: auto; margin-right: auto; width: 40%;" class="d-block img-fluid" src="resources/uploads/mac2.png" alt="Second slide">
-                            <h3>Apple Macbook Air</h3>
-                    </div>
-                    <div class="carousel-item">
-                        <img style="display: block; margin-left: auto; margin-right: auto; width: 40%;" class="d-block img-fluid" src="resources/uploads/surface.png" alt="Third slide">
-                            <h3>Surface Pro</h3>
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>
-
             <div class="row">
+                <h1>Shop Featured Products</h1>
+            </div>
+                <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner" role="listbox">
+                        <div class="carousel-item active">
+                            <img style="display: block; margin-left: auto; margin-right: auto; width: 30%;" class="d-block img-fluid" src="resources/uploads/dell2.png" alt="First slide">
+                            <h3>Dell Inspiron 3000</h3>
+                        </div>
+                        <div class="carousel-item">
+                            <img style="display: block; margin-left: auto; margin-right: auto; width: 30%;" class="d-block img-fluid" src="resources/uploads/mac2.png" alt="Second slide">
+                            <h3>Apple Macbook Air</h3>
+                        </div>
+                        <div class="carousel-item">
+                            <img style="display: block; margin-left: auto; margin-right: auto; width: 30%;" class="d-block img-fluid" src="resources/uploads/surface.png" alt="Third slide">
+                            <h3>Surface Pro</h3>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
 
-                <?php
+
+                <div class="row">
+                    <?php
     if(isset($_SESSION['useraccount']) && $_SESSION['useraccount'][1] == 'buyer'){
-        include(TEMPLATE_BACK . "/recommended.php");}else
-    {
-        get_products();
-    }?>
+        include(TEMPLATE_BACK . "/recommended.php");
+    }else
+    {?>
+                    <div class="row">
+                        <h1>Shop Our Products</h1>
+                        <div class="row">
+                            <?php get_products();?>
+                        </div>
+                    </div>
+                    <?php } ?>
+
+                </div>
+                <!-- /.row -->
 
             </div>
-            <!-- /.row -->
+            <!-- /.col-lg-9 -->
 
         </div>
-        <!-- /.col-lg-9 -->
+        <!-- /.row -->
 
     </div>
-    <!-- /.row -->
+    <!-- /.container -->
 
-</div>
-<!-- /.container -->
-
-<!-- Footer -->
-<?php    include(TEMPLATE_FRONT . DS . "footer.php")?>
+    <!-- Footer -->
+    <?php    include(TEMPLATE_FRONT . DS . "footer.php")?>

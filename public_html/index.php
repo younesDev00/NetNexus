740d1b1fbd1888cfb47f07e3a9aaaa040dec9305
Gabrielle Walker <13196732@student.uts.edu.au>
2019-10-11 17:11:04 +1100

    personalised home page

--- public_html/index.php
+++ public_html/index.php
@@ -1,38 +1,73 @@
-<?php require_once("../resources/config.php"); ?>
-
-<?php include(TEMPLATE_FRONT . DS . "header.php"); ?>
-
+<?php
+require_once("../resources/config.php");
+include(TEMPLATE_FRONT . DS . "header.php")
+?>
 
 <!-- Page Content -->
 <div class="container">
 
     <div class="row">
 
+        <div class="col-lg-3">
 
-            <div class="col-md-10">
+            <?php    include(TEMPLATE_FRONT . DS . "side_nav.php")?>
 
-                <div class="row carousel-holder">
 
-                    <div class="col-md-12">
+        </div>
+        <!-- /.col-lg-3 -->
 
-                      <?php include(TEMPLATE_FRONT . DS . "slider.php") ?>
+        <div class="col-lg-9">
 
+            <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner" role="listbox">
+                    <div class="carousel-item active">
+                        <img style="display: block; margin-left: auto; margin-right: auto; width: 40%;" class="d-block img-fluid" src="resources/uploads/dell2.png" alt="First slide">
+                            <h3>Dell Inspiron 3000</h3>
                     </div>
-
+                    <div class="carousel-item">
+                        <img style="display: block; margin-left: auto; margin-right: auto; width: 40%;" class="d-block img-fluid" src="resources/uploads/mac2.png" alt="Second slide">
+                            <h3>Apple Macbook Air</h3>
+                    </div>
+                    <div class="carousel-item">
+                        <img style="display: block; margin-left: auto; margin-right: auto; width: 40%;" class="d-block img-fluid" src="resources/uploads/surface.png" alt="Third slide">
+                            <h3>Surface Pro</h3>
+                    </div>
                 </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
 
+            <div class="row">
 
-
-
-
-                <div class="row">
-                    <?php get_products(); ?>
-                </div><!-- ROw ends here-->
+                <?php
+    if(isset($_SESSION['useraccount']) && $_SESSION['useraccount'][1] == 'buyer'){
+        include(TEMPLATE_BACK . "/recommended.php");}else
+    {
+        get_products();
+    }?>
 
             </div>
-        </div>
-    </div>
+            <!-- /.row -->
 
-    <!-- /.container -->
-    <!--///.lk-->
-    <?php include(TEMPLATE_FRONT . DS . "footer.php") ?>
+        </div>
+        <!-- /.col-lg-9 -->
+
+    </div>
+    <!-- /.row -->
+
+</div>
+<!-- /.container -->
+
+<!-- Footer -->
+<?php    include(TEMPLATE_FRONT . DS . "footer.php")?>
